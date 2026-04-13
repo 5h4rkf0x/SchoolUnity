bab4976831138cfc58cb146d0f1b2e65f74bc2aa
5h4rkf0x <sharkfox1014@gmail.com>
2026-04-14 00:03:08 +0900

    점프 애니메이션 완

--- BossFPSProject/Assets/Scripts/Player/PlayerMove.cs
+++ BossFPSProject/Assets/Scripts/Player/PlayerMove.cs
@@ -1,20 +1,31 @@
 using JetBrains.Rider.Unity.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    // Move
     [SerializeField] private float moveSpeed;
     [SerializeField] private float sprintSpeed;
-    [SerializeField] private bool isSprinting;
+    
+    private Vector2 moveInput;
+    private Vector3 moveDir;
+    private bool isSprinting;
 
-    [SerializeField] private Vector3 moveDir;
+    // Jump
+    [SerializeField] private float jumpForce;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool isGrounded;
+
+
     [SerializeField] private Rigidbody rigid;
     [SerializeField] private PlayerInput playerInput;
     [SerializeField] private Transform playerCam;
     [SerializeField] private Animator animator;
-
-    [SerializeField] private Vector2 moveInput;
 
     public void OnMove(InputValue value)
     {
@@ -35,11 +46,15 @@ public class PlayerMove : MonoBehaviour
     {
         isSprinting = playerInput.actions["Sprint"].IsPressed();
         Move();
+        CheckGround();
+    }
+    private void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere( groundCheck.position, groundCheckRadius, groundLayer );
     }
 
     private void Move()
     {
-
         Vector3 cameraForward = playerCam.forward;
         Vector3 cameraRight = playerCam.right;
 
@@ -52,12 +67,22 @@ public class PlayerMove : MonoBehaviour
 
         float currentSpeed = isSprinting ? sprintSpeed : moveSpeed;
 
-        // rigid.linearVelocity = moveDir * currentSpeed;
         Vector3 velocity = rigid.linearVelocity;
         velocity.x = moveDir.x * currentSpeed;
         velocity.z = moveDir.z * currentSpeed;
         rigid.linearVelocity = velocity;
+    }
 
+    public void OnJump(InputValue value)
+    {
+        if (!value.isPressed) return;
+
+        if (isGrounded)
+        {
+            rigid.linearVelocity = new Vector3(rigid.linearVelocity.x, 0f, rigid.linearVelocity.z);
+
+            rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     private void AnimationControl()
@@ -69,5 +94,7 @@ public class PlayerMove : MonoBehaviour
         animator.SetFloat("Velocity", Velocity);
         animator.SetFloat("DirX", moveInput.x);
         animator.SetFloat("DirY", moveInput.y);
+
+        animator.SetBool("IsJumping", !isGrounded);
     }
 }
